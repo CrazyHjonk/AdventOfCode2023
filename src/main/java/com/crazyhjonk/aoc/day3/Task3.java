72a7f0a727c16c3f2a650bc0de6b960b8e67b0ad
CrazyHjonk <CrazyHjonk@gmail.com>
2023-12-03 11:58:45 +0100

    Day 3 part 2 done

--- src/main/java/com/crazyhjonk/aoc/day3/Task3.java
+++ src/main/java/com/crazyhjonk/aoc/day3/Task3.java
@@ -8,6 +8,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Task3 {
 
@@ -32,10 +34,10 @@ public class Task3 {
             throw new RuntimeException(e);
         }
         System.out.println("Part 1: " + processMap(items));
-        System.out.println("Part 2: ");
+        System.out.println("Part 2: " + processMapPt2(items));
     }
 
-    public static int processMap(char[][] items) {
+    static int processMap(char[][] items) {
         int sum = 0;
         for (int i = 0; i < items.length; i++) {
             char[] item = items[i];
@@ -51,7 +53,7 @@ public class Task3 {
                 }
                 boolean isAdjacent = false;
                 for (int k = j - 1; k >= originalJ; k--) {
-                    if (isAdjacentToSymbol(items, i, k)) {
+                    if (isAdjacentToSymbol(i, k)) {
                         isAdjacent = true;
                         break;
                     }
@@ -64,7 +66,57 @@ public class Task3 {
         return sum;
     }
 
-    static boolean isAdjacentToSymbol(char[][] items, int x, int y) {
+    static int processMapPt2(char[][] items) {
+        int sum = 0;
+        for (int i = 0; i < items.length; i++) {
+            char[] item = items[i];
+            for (int j = 0; j < item.length; j++) {
+                if (item[j] != '*') continue;
+                int[] adjacentPartNumbers = getAdjacentPartNumbers(i, j);
+                if (adjacentPartNumbers.length != 2) continue;
+                sum += adjacentPartNumbers[0] * adjacentPartNumbers[1];
+            }
+        }
+        return sum;
+    }
+
+    static int[] getAdjacentPartNumbers(int x, int y) {
+        int[] numbers = new int[8];
+        int k = 0;
+        for (int i = x - 1; i <= x + 1; i++) {
+            if (i < 0 || i >= items.length) continue;
+            for (int j = y - 1; j <= y + 1; j++) {
+                Map<Integer, Character> digits = new HashMap<>();
+                if (j < 0 || j >= items[i].length) continue;
+                if (i == x && j == y) continue;
+                if (Character.isDigit(items[i][j])) {
+                    digits.put(0, items[i][j]);
+                    if (Character.isDigit(items[i][j-1])) {
+                        digits.put(-1, items[i][j-1]);
+                        if (Character.isDigit(items[i][j-2])) {
+                            digits.put(-2, items[i][j-2]);
+                        }
+                    }
+                    if (Character.isDigit(items[i][j+1])) {
+                        digits.put(1, items[i][j+1]);
+                        if (Character.isDigit(items[i][j+2])) {
+                            digits.put(2, items[i][j+2]);
+                            j++;
+                        }
+                        j++;
+                    }
+
+                    String number = digits.entrySet().stream().sorted(Map.Entry.comparingByKey()).map(Map.Entry::getValue)
+                        .map(Object::toString).reduce("", String::concat);
+                    numbers[k] = Integer.parseInt(number);
+                }
+                k++;
+            }
+        }
+        return Arrays.stream(numbers).filter(i -> i != 0).toArray();
+    }
+
+    static boolean isAdjacentToSymbol(int x, int y) {
         for (int i = x - 1; i <= x + 1; i++) {
             if (i < 0 || i >= items.length) continue;
             for (int j = y - 1; j <= y + 1; j++) {

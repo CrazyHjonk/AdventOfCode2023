762f9de81c7f5eeced5a255491bc9905e63a36d0
CrazyHjonk <CrazyHjonk@gmail.com>
2023-12-08 13:34:18 +0100

    Day 8 part 2 done

--- src/main/java/com/crazyhjonk/aoc/day8/Task8.java
+++ src/main/java/com/crazyhjonk/aoc/day8/Task8.java
@@ -1,20 +1,18 @@
 package com.crazyhjonk.aoc.day8;
 
 import com.crazyhjonk.aoc.day1.Task1;
+import com.crazyhjonk.aoc.util.MathUtil;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Task8 {
 
-    private static int steps;
-    private static String instructions;
+    private static long steps;
+    private static char[] instructions;
     private static final Map<String, Mapping> mappings = new HashMap<>();
 
     static class Mapping {
@@ -31,7 +29,14 @@ public class Task8 {
             this.rightString = rightString;
         }
 
-
+        @Override
+        public String toString() {
+            return "Mapping{" +
+                "name='" + name + '\'' +
+                ", leftString='" + leftString + '\'' +
+                ", rightString='" + rightString + '\'' +
+                '}';
+        }
     }
 
 
@@ -47,7 +52,7 @@ public class Task8 {
             while (line != null) {
                 lines.add(line);
                 if (lines.size() == 1) {
-                    instructions = line;
+                    instructions = line.toCharArray();
                     line = reader.readLine();
                     continue;
                 }
@@ -64,6 +69,9 @@ public class Task8 {
 
             registerLR();
             run(mappings.get("AAA"));
+            steps = 0;
+            //run2(init2());
+            run2attempt2(init2());
 
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -84,7 +92,7 @@ public class Task8 {
     }
 
     private static void run(Mapping current) {
-        for (char c : instructions.toCharArray()) {
+        for (char c : instructions) {
             steps++;
             if (c == 'L') {
                 current = current.left;
@@ -96,6 +104,63 @@ public class Task8 {
             System.out.println("Part 1: " + steps);
         } else {
             run(current);
+        }
+    }
+
+    private static Mapping[] init2() {
+        List<Mapping> current = new ArrayList<>();
+        for (Mapping mapping : mappings.values()) {
+            if (mapping.name.endsWith("A")) {
+                current.add(mapping);
+            }
+        }
+        return current.toArray(Mapping[]::new);
+    }
+
+    private static void run2(Mapping[] current) {
+        while (true) {
+            for (char c : instructions) {
+                steps++;
+                for (int i = 0; i < current.length; i++) {
+                    if (c == 'L') {
+                        current[i] = current[i].left;
+                    } else {
+                        current[i] = current[i].right;
+                    }
+                }
+                if (Arrays.stream(current).allMatch(m -> m.name.endsWith("Z"))) {
+                    System.out.println("Part 2: " + steps);
+                    return;
+                }
+            }
+            System.out.println(steps);
+        }
+    }
+
+    private static void run2attempt2(Mapping[] mappings) {
+        long[] steps = new long[mappings.length];
+        for (int i = 0; i < mappings.length; i++) {
+            steps[i] = getSteps(mappings[i]);
+        }
+
+        System.out.println("Part 2: " + MathUtil.lcm(steps));
+
+    }
+
+    private static int getSteps(Mapping mapping) {
+        int steps = 0;
+        while (true) {
+            for (char c : instructions) {
+                steps++;
+                if (c == 'L') {
+                    mapping = mapping.left;
+                } else {
+                    mapping = mapping.right;
+                }
+                if (mapping.name.endsWith("Z")) {
+                    return steps;
+                }
+            }
         }
     }
 }
